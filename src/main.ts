49e4bf4c90d00874e397cbcb70dfceaba317caf0
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-06-01 16:45:59 -0700

    format files
    Former-commit-id: 2fc4bc18d3014b0c4b0be0d41e79e97152a2c529

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,65 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-import modelURL from '/src/models/akshat.glb?url';
+import modelURL from "/src/models/akshat.glb?url";
 import "./style.css";
 
 // Setup
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 // Move camera back
 camera.position.z = 17;
 
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector<HTMLCanvasElement>("#bg")!});
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector<HTMLCanvasElement>("#bg")!,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const loader = new GLTFLoader();
 
 // Load name model
 let nameObject: THREE.Group;
-loader.load(modelURL, function(model) {
-  console.log("Loaded model");
-  nameObject = model.scene;
-  nameObject.scale.set(5, 5, 5);
-  // rotate 90 degrees or pi/2 radians
-  nameObject.rotateX(Math.PI/2);
-  scene.add(nameObject);
+loader.load(modelURL, function (model) {
+	console.log("Loaded model");
+	nameObject = model.scene;
+	nameObject.scale.set(5, 5, 5);
+	// rotate 90 degrees or pi/2 radians
+	nameObject.rotateX(Math.PI / 2);
+	scene.add(nameObject);
 });
 
 // Lighting
-const darkBluePointLight = new THREE.PointLight(0x4942FF);
+const darkBluePointLight = new THREE.PointLight(0x4942ff);
 darkBluePointLight.intensity = 0.35;
 darkBluePointLight.position.set(15, 0, 15);
 
-const lightBluePointLight = new THREE.PointLight(0x1DAFFD);
+const lightBluePointLight = new THREE.PointLight(0x1daffd);
 lightBluePointLight.intensity = 0.35;
 lightBluePointLight.position.set(0, 0, 15);
 
-const greenPointLight = new THREE.PointLight(0x69F61E);
+const greenPointLight = new THREE.PointLight(0x69f61e);
 greenPointLight.intensity = 0.35;
 greenPointLight.position.set(-15, 0, 15);
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.2 );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
 directionalLight.position.set(0, 0, -30);
-scene.add( directionalLight );
-
+scene.add(directionalLight);
 
 scene.add(darkBluePointLight, lightBluePointLight, greenPointLight);
 
 // Animation loop
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
-  if(nameObject) {
-    nameObject.rotation.y += 0.01
-    nameObject.rotation.z += 0.01
-  }
-	renderer.render( scene, camera );
+	if (nameObject) {
+		nameObject.rotation.y += 0.01;
+		nameObject.rotation.z += 0.01;
+	}
+	renderer.render(scene, camera);
 }
 animate();
