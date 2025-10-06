55d8c484db124929cf642d30dc232c6b7ade6ee1
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-10-06 11:54:44 -0700

    fix text contrast issues

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -30,7 +30,7 @@ export function Header() {
             >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
               </span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 About
               </span>
             </a>
@@ -40,7 +40,7 @@ export function Header() {
             >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
               </span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 experience
               </span>
             </a>
