ec4de6d8d7017c8c2f2376b11cba74c0c5ee0ca7
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-10-10 23:01:05 -0700

    display link in header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -20,12 +20,15 @@ export function Header() {
           <li className="mr-5 text-xs shrink-0">
             <LinkedIn />
           </li>
+          <li className="mr-5 text-xs shrink-0">
+            <Aperture />
+          </li>
         </ul>
         <nav className="nav hidden lg:block">
           <ul className="mt-16 w-max">
             <a className="group flex items-center py-3" href="#about">
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 About
               </span>
             </a>
@@ -34,7 +37,7 @@ export function Header() {
               href="#experience"
             >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 experience
               </span>
             </a>
