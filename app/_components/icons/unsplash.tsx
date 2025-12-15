8475a5e33dd65fa110be16f22c3151c557f5d8b7
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-12-14 18:13:52 -0800

    fix jsx property name

--- app/_components/icons/unsplash.tsx
+++ app/_components/icons/unsplash.tsx
@@ -17,7 +17,7 @@ export function Unsplash() {
       >
         <path
           d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"
-          fill-rule="nonzero"
+          fillRule="nonzero"
         />
       </svg>
     </a>
